360cc06df710f737ce6a468bda60e2d52c626773
Yevheniy Oliynyk <evgen41293@gmail.com>
2018-04-09 10:49:02 +0300

    Refactored, removed inner classes, used lambdas

--- src/main/java/org/evgeniy/ua/server/Nio2EchoServer.java
+++ src/main/java/org/evgeniy/ua/server/Nio2EchoServer.java
@@ -6,6 +6,7 @@ import java.nio.channels.AsynchronousServerSocketChannel;
 import java.nio.channels.AsynchronousSocketChannel;
 import java.nio.channels.CompletionHandler;
 import java.nio.charset.StandardCharsets;
+import java.util.function.BiConsumer;
 
 public class Nio2EchoServer implements Server {
 
@@ -21,22 +22,7 @@ public class Nio2EchoServer implements Server {
         try (AsynchronousServerSocketChannel asynchronousServerSocketChannel = AsynchronousServerSocketChannel.open()) {
             if (asynchronousServerSocketChannel.isOpen()) {
                 asynchronousServerSocketChannel.bind(new InetSocketAddress(port));
-                asynchronousServerSocketChannel.accept(
-                        null,
-                        new CompletionHandler<AsynchronousSocketChannel, Void>() {
-
-                            @Override
-                            public void completed(AsynchronousSocketChannel client, Void attachment) {
-                                asynchronousServerSocketChannel.accept(null, this);
-                                accept(client);
-                            }
-
-                            @Override
-                            public void failed(Throwable exc, Void attachment) {
-                                asynchronousServerSocketChannel.accept(null, this);
-                                throw new UnsupportedOperationException("Cannot accept connections!");
-                            }
-                        });
+                asynchronousServerSocketChannel.accept(null, acceptCallback(asynchronousServerSocketChannel));
                 // Infinite loop
                 for (; ; ) {
                     Thread.sleep(1000);
@@ -45,55 +31,72 @@ public class Nio2EchoServer implements Server {
         }
     }
 
-    private static void accept(AsynchronousSocketChannel client) {
-        try {
-            // As async channels API have own thread pool, byte buffer should be per client connection
-            ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
-            buffer.put(String.format(WELCOME_MESSAGE_TEMPLATE, client.getRemoteAddress()).getBytes());
-            buffer.flip();
-            client.write(buffer, null, new CompletionHandler<Integer, Object>() {
-                @Override
-                public void completed(Integer result, Object attachment) {
-                    buffer.clear();
-                    doRead(client, buffer);
+    private static Callback<AsynchronousSocketChannel, Object> acceptCallback(AsynchronousServerSocketChannel asynchronousServerSocketChannel) {
+        return new Callback<>(
+                (client, attachment1) -> {
+                    asynchronousServerSocketChannel.accept(null, acceptCallback(asynchronousServerSocketChannel));
+                    try {
+                        // As async channels API have own thread pool, byte buffer should be per client connection
+                        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
+                        buffer.put(String.format(WELCOME_MESSAGE_TEMPLATE, client.getRemoteAddress()).getBytes());
+                        buffer.flip();
+                        client.write(buffer, null, new Callback<>(
+                                (bytesWritten, attachment2) -> {
+                                    buffer.clear();
+                                    doRead(client, buffer);
+                                }
+                        ));
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                },
+                (exc, attachment) -> {
+                    asynchronousServerSocketChannel.accept(null, acceptCallback(asynchronousServerSocketChannel));
+                    throw new UnsupportedOperationException("Cannot accept connections!");
                 }
-
-                @Override
-                public void failed(Throwable exc, Object attachment) {
-                    exc.printStackTrace();
-                }
-            });
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        );
     }
 
     private static void doRead(AsynchronousSocketChannel client, ByteBuffer buffer) {
-        client.read(buffer, null, new CompletionHandler<Integer, Object>() {
-            @Override
-            public void completed(Integer result, Object attachment) {
-                buffer.flip();
-                String clientMessage = StandardCharsets.UTF_8.decode(buffer).toString();
-                String response = String.format(READ_DATA_MESSAGE_TEMPLATE, clientMessage);
-                ByteBuffer bb = ByteBuffer.wrap(response.getBytes());
-                buffer.clear();
-                client.write(bb, null, new CompletionHandler<Integer, Object>() {
-                    @Override
-                    public void completed(Integer result, Object attachment) {
-                        doRead(client, buffer);
-                    }
+        client.read(buffer, null, new Callback<>(
+                (bytesRead, attachment1) -> {
+                    buffer.flip();
+                    String clientMessage = StandardCharsets.UTF_8.decode(buffer).toString();
+                    String response = String.format(READ_DATA_MESSAGE_TEMPLATE, clientMessage);
+                    ByteBuffer bb = ByteBuffer.wrap(response.getBytes());
+                    client.write(bb, null, new Callback<>(
+                            (bytesWritten, attachment2) -> {
+                                buffer.clear();
+                                doRead(client, buffer);
+                            }
+                    ));
+                }
+        ));
+    }
 
-                    @Override
-                    public void failed(Throwable exc, Object attachment) {
-                        exc.printStackTrace();
-                    }
-                });
-            }
+    private static class Callback<V, A> implements CompletionHandler<V, A> {
 
-            @Override
-            public void failed(Throwable exc, Object attachment) {
-                exc.printStackTrace();
-            }
-        });
+        private final BiConsumer<V, A> completedHandler;
+        private final BiConsumer<Throwable, A> failedHandler;
+
+        private Callback(BiConsumer<V, A> completedHandler) {
+            this(completedHandler, (exc, attachment) -> exc.printStackTrace());
+        }
+
+        private Callback(BiConsumer<V, A> completedHandler, BiConsumer<Throwable, A> failedHandler) {
+            this.completedHandler = completedHandler;
+            this.failedHandler = failedHandler;
+        }
+
+
+        @Override
+        public void completed(V result, A attachment) {
+            completedHandler.accept(result, attachment);
+        }
+
+        @Override
+        public void failed(Throwable exc, A attachment) {
+            failedHandler.accept(exc, attachment);
+        }
     }
 }
